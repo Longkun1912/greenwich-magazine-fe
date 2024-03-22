434be43954af0d532d0b070b08b297f3c3fc965d
Long Hoang <rongvang1912tc@gmail.com>
2024-03-22 20:52:10 +0700

    Configure deployment

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import Login from "./components/Login";
 import Profile from "./components/Profile";
 import Register from "./components/Register";
 import UnAuthorizedPage from "./components/Unauthorized";
+import ContribbutionIndex from "./components/contribution/index";
+import FacultyIndex from "./components/faculty/index";
 import UserIndex from "./components/user/index";
 import GreenwichNavBar from "./global/Navbar";
 import Sidebar from "./global/Sidebar";
 import AuthService from "./services/auth.service";
-import FacultyIndex from "./components/faculty/index";
-import ContribbutionIndex from "./components/contribution/index";
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
@@ -46,6 +46,10 @@ function App() {
             {currentUser && <Sidebar currentUser={currentUser} />}
             <Routes>
               <Route
+                path="/"
+                element={<Login onLoginSuccess={handleLoginSuccess} />}
+              />
+              <Route
                 path="/profile"
                 element={<Profile currentUser={currentUser} />}
               />
@@ -57,7 +61,10 @@ function App() {
               />
               <Route path="/register" element={<Register />} />
               <Route path="/faculty" element={<FacultyIndex />} />
-              <Route path="/contributionIndex" element={<ContribbutionIndex/>} />
+              <Route
+                path="/contributionIndex"
+                element={<ContribbutionIndex />}
+              />
               <Route
                 path="/user-management"
                 element={<UserIndex currentUser={currentUser} />}
